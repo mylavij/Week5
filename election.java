52e870d2bded732ce58c3cc8d1aeda0df294fc21
mylavij <159325680+mylavij@users.noreply.github.com>
2024-03-04 15:00:27 -0500

    Create election.java
    uuuu

---
+++ election.java
@@ -0,0 +1,14 @@
+/***
+ Name: Myla Vijayan
+ Date: 3/4/24
+ Program: Java
+ */
+
+public class election 
+
+public static void main(String[] args)
+{
+    Scanner.input = new Scanner 
+    System.out.println("The results of a primary election between two candidates in three states are:");
+    System.out.println("");
+}
