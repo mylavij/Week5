581fec5829eaefe8e28b9b9b67a920113ee81f65
mylavij <159325680+mylavij@users.noreply.github.com>
2024-03-05 15:00:01 -0500

    fsdfs
    uiou

--- election.java
+++ election.java
@@ -4,11 +4,24 @@
  Program: Java
  */
 
-public class election 
+import java.util.*;
 
-public static void main(String[] args)
+public class election 
 {
-    Scanner.input = new Scanner 
+public static void main(String[] args)
+{ 
     System.out.println("The results of a primary election between two candidates in three states are:");
+    System.out.println("\n\t\t\t\t     Awbrey   Martinez\n\t\t\tNew York     314159   271860\n\t\t\tNew Jersey   89008    121032\n\t\t\tConnecticut  213451   231034");
+    
+    Scanner.input = new Scanner
     System.out.println("");
+    int awbreyVotes;
+    double awbreyPercentage;
+    int martinezVotes;
+    double martinezPercentage;
+    int totalVotes;
+
 }
+
+}
+
